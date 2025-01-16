3f011a21168972f04c662c5a9995783647c9050b
dev-l4m4 <dev.sun5066@gmail.com>
2025-01-16 11:59:30 +0900

    feature: riot api setting (#7)
    - [x] API key 변경
    - [x] 아시아 서버, 한국 서버 각각 Retrofit 생성

---
+++ core/di/src/main/java/com/sun5066/di/retrofits/ProvidesRetrofit.kt
@@ -0,0 +1,11 @@
+package com.sun5066.di.retrofits
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AsiaRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class KoreaRetrofit
