b2e615e0a7f4160b38c1dea820cb35e92b32f9a5
dev-l4m4 <dev.sun5066@gmail.com>
2025-01-21 17:50:44 +0900

    refactor: CommonViewModel.kt 예외 처리 개선
    - API Response 실패 응답 코드 상수에 Response 코드 명시
    - safeLaunch의 onFailure에 CancellationException 예외 처리 추가

--- core/base/src/main/java/com/sun5066/base/mvi/CommonViewModel.kt
+++ core/base/src/main/java/com/sun5066/base/mvi/CommonViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.sun5066.base.R
 import com.sun5066.config.Constants
 import com.sun5066.config.exception.ApiException
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -24,14 +25,14 @@ abstract class CommonViewModel<INTENT : Any, STATE : Parcelable>(
 
     private fun apiExceptionHandle(responseCode: Int) {
         when (responseCode) {
-            Constants.RESPONSE_CODE_BAD_REQUEST -> R.string.common_msg_error_bad_request
-            Constants.RESPONSE_CODE_UNAUTHORIZED -> R.string.common_msg_error_unauthorized
-            Constants.RESPONSE_CODE_FORBIDDEN -> R.string.common_msg_error_forbidden
-            Constants.RESPONSE_CODE_NOT_FOUND -> R.string.common_msg_error_not_found
-            Constants.RESPONSE_CODE_UNSUPPORTED_MEDIA_TYPE -> R.string.common_msg_error_unsupported_media_type
-            Constants.RESPONSE_CODE_RATE_LIMIT_EXCEEDED -> R.string.common_msg_error_rate_limit_exceeded
-            Constants.RESPONSE_CODE_INTERNAL_SERVER_ERROR -> R.string.common_msg_error_internal_server_error
-            Constants.RESPONSE_CODE_SERVICE_UNAVAILABLE -> R.string.common_msg_error_service_unavailable
+            Constants.RESPONSE_CODE_400_BAD_REQUEST -> R.string.common_msg_error_bad_request
+            Constants.RESPONSE_CODE_401_UNAUTHORIZED -> R.string.common_msg_error_unauthorized
+            Constants.RESPONSE_CODE_403_FORBIDDEN -> R.string.common_msg_error_forbidden
+            Constants.RESPONSE_CODE_404_NOT_FOUND -> R.string.common_msg_error_not_found
+            Constants.RESPONSE_CODE_415_UNSUPPORTED_MEDIA_TYPE -> R.string.common_msg_error_unsupported_media_type
+            Constants.RESPONSE_CODE_429_RATE_LIMIT_EXCEEDED -> R.string.common_msg_error_rate_limit_exceeded
+            Constants.RESPONSE_CODE_500_INTERNAL_SERVER_ERROR -> R.string.common_msg_error_internal_server_error
+            Constants.RESPONSE_CODE_503_SERVICE_UNAVAILABLE -> R.string.common_msg_error_service_unavailable
             else -> R.string.common_msg_error_network
         }.also { postSideEffect(CommonSideEffect.ShowSnackBarRes(it)) }
     }
@@ -56,6 +57,8 @@ abstract class CommonViewModel<INTENT : Any, STATE : Parcelable>(
             }
             block()
         }.onFailure { throwable ->
+            if (throwable is CancellationException) return@onFailure
+
             error?.invoke(throwable) ?: commonErrorHandle(throwable)
         }
     }.apply {
